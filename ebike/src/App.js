da950c48e06b8d390ffc4791dc26fd451e5f1eee
Gigibot <55855355+gigibot-tech@users.noreply.github.com>
2022-10-06 21:53:31 +0300

    push

--- ebike/src/App.js
+++ ebike/src/App.js
@@ -6,12 +6,6 @@ import HomeController from './components/HomeController';
 function App() {
   return (
     <div className="App">
-    <iframe 
-      width="100%"
-      length="100%"
-      src="http://localhost:3000/BikeCustomizer/index.html"
-    >
-    </iframe>
       <NavbarH  />
       <HomeController />
     </div>
--- ebike/src/components/HomeController.js
+++ ebike/src/components/HomeController.js
@@ -1,18 +1,33 @@
 import React from "react"
 import { useState, useEffect, useCallback } from "react";
 
+import { fetchGet } from '../fetch/fetchData'
+import { fetchGetWithQuery } from '../fetch/fetchData'
+
 
 function HomeController() {
+    let [custom,setCustom] = useState("")
+
+    const fetchData = useCallback(async () => {
+        console.log('fetching data....')
+        const rsp2 = await fetchGetWithQuery('https://ebikepred.cognitiveservices.azure.com/luis/prediction/v3.0/apps/473772f7-9ffc-4ba1-81d7-03ab9868c297/slots/staging/predict?verbose=true&show-all-intents=true&log=true&subscription-key=61961a1cb3054d20b11d5daa96d68e79&query=switch','')
+        const rsp = await fetchGetWithQuery('http://localhost:5000/api/v1/getAllItems/FoodItems/','')       
+        const tmp = await rsp.json();
+        //setCustom(tmp.data)
+        console.log(tmp)
+        //return tmp.data.prediction
+    })
+    
+    //switch().topIntent
+
+    //.intents.map intent,v  v.score
+
+    useEffect(() => {
+        fetchData()
+    },[fetchData])       
+
     return(
     <div>
-        <div>
-    <iframe 
-      width="100%"
-      length="100%"
-      src="http://localhost:3000/BikeCustomizer/index.html"
-    >
-    </iframe>
-    </div>
         <div>test</div>
     </div>
     )
